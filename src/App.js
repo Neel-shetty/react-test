417a21281fd45d222c7691d0c18f43994c841743
Neel <neelnarayanshetty@gmail.com>
2022-11-19 23:11:54 +0530

    trying

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React,{ useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState()
+
+  function Incraese(){
+    setCounter(counter+1)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Nigga</code> and save to reload.
         </p>
+        <button onClick={Incraese}>Press header</button>
+        <p>{Incraese(0)}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
